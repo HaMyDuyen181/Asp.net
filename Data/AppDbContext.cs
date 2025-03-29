02739ec00500f35cdaeb5f85bf0abcc21a93f6c7
HaMyDuyen181 <beeduyen181@gmail.com>
2025-03-29 10:48:04 +0700

    update 1

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using HaMyDuyen_2122110528.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace HaMyDuyen_2122110528.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; } 
+
+    }
+}
